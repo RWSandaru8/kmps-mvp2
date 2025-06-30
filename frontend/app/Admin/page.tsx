b3803889432ed75aace56333de1c7e95b2dca5d7
Dilesha Gayashan <m20010808008@student.cinec.edu>
2025-06-30 15:27:35 +0530

    storage card

--- frontend/app/Admin/page.tsx
+++ frontend/app/Admin/page.tsx
@@ -8,6 +8,7 @@ import { AuthContext } from '@/context/auth-context';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
+import StorageCard from '@/components/StorageCard';
 
 
 // Register Chart.js components - including DoughnutController
@@ -596,6 +597,7 @@ const DentalDashboard: React.FC = () => {
               </Card>
             );
           })}
+          <StorageCard/>
         </div>
 
         {/* Main Analytics Section */}
